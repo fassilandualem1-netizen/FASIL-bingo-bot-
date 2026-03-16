dc9e88d8cf484b82f36a005553322fae7665d4ab
fassilandualem1-netizen <fassilandualem1@gmail.com>
2026-03-16 12:18:09 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,12 @@
-import telebot
 import os
-from flask import Flask
-from threading import Thread
-# ... የተቀረው ኮድ
+import telebot
+
+# Token-ን ከ Environment Variable ላይ ያነባል (ለደህንነት)
+BOT_TOKEN = os.environ.get('BOT_TOKEN')
+bot = telebot.TeleBot(BOT_TOKEN)
+
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "ሰላም! ቦቱ በ Render ላይ እየሰራ ነው።")
+
+bot.infinity_polling()
